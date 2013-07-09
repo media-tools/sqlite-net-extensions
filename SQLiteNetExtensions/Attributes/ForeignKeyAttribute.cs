397d1b0223e65e1a052a98cce0b9c663dc5876c1
Guillermo Gutiérrez Doral <guillermo.gutierrez@twincoders.com>
2013-07-09 14:26:25 +0200

    ForeignKey attribute now inherits from Indexed attribute

--- SQLiteNetExtensions/Attributes/ForeignKeyAttribute.cs
+++ SQLiteNetExtensions/Attributes/ForeignKeyAttribute.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 
+#if USING_MVVMCROSS
+using Cirrious.MvvmCross.Plugins.Sqlite;
+#else
+using SQLite;
+#endif
+
 namespace SQLiteNetExtensions.Attributes
 {
     [AttributeUsage(AttributeTargets.Property)]
-    public class ForeignKeyAttribute : Attribute
+    public class ForeignKeyAttribute : IndexedAttribute
     {
         public ForeignKeyAttribute(Type foreignType)
         {
