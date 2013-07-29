02988a58c625e5574c1a7cd7322d0f6a4acd3eb9
Guillermo Gutiérrez Doral <guillermo.gutierrez@twincoders.com>
2013-07-29 14:54:23 +0200

    - Implemented read and write operations with TextBlob relationships - Implemented default TextBlob serializer using json-net

---
+++ SQLiteNetExtensions/Attributes/TextBlobAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SQLiteNetExtensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class TextBlobAttribute : RelationshipAttribute
+    {
+        public TextBlobAttribute(string textProperty) : base(null, null, null, OnDeleteAction.None)
+        {
+            TextProperty = textProperty;
+        }
+
+        public string TextProperty { get; private set; }
+    }
+}
---
+++ SQLiteNetExtensions/Extensions/TextBlob/TextBlobOperations.cs
@@ -0,0 +1,64 @@
+﻿#if USING_MVVMCROSS
+using System.Diagnostics;
+using System.Reflection;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions.TextBlob.Serializers;
+using SQLiteConnection = Cirrious.MvvmCross.Plugins.Sqlite.ISQLiteConnection;
+#else
+using SQLite;
+#endif
+
+
+namespace SQLiteNetExtensions.Extensions.TextBlob
+{
+    public static class TextBlobOperations
+    {
+        private static ITextBlobSerializer _serializer;
+
+        public static void SetTextSerializer(ITextBlobSerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public static ITextBlobSerializer GetTextSerializer()
+        {
+            // If not specified, use default JSON serializer
+            return _serializer ?? (_serializer = new JsonBlobSerializer());
+        }
+
+        public static void GetTextBlobChild<T>(ref T element, PropertyInfo relationshipProperty)
+        {
+            var type = typeof(T);
+            var relationshipType = relationshipProperty.PropertyType;
+
+            Debug.Assert(relationshipType == typeof(string), "TextBlob property is already a string");
+
+            var textblobAttribute = relationshipProperty.GetAttribute<TextBlobAttribute>();
+            var textProperty = type.GetProperty(textblobAttribute.TextProperty, typeof (string));
+            Debug.Assert(textProperty != null, "Text property for TextBlob relationship not found");
+
+            var textValue = (string)textProperty.GetValue(element, null);
+            var value = textValue != null ? GetTextSerializer().Deserialize(textValue, relationshipType) : null;
+
+            relationshipProperty.SetValue(element, value, null);
+        }
+
+        public static void UpdateTextBlobProperty<T>(ref T element, PropertyInfo relationshipProperty)
+        {
+            var type = typeof(T);
+            var relationshipType = relationshipProperty.PropertyType;
+
+            Debug.Assert(relationshipType == typeof(string), "TextBlob property is already a string");
+
+            var textblobAttribute = relationshipProperty.GetAttribute<TextBlobAttribute>();
+            var textProperty = type.GetProperty(textblobAttribute.TextProperty, typeof(string));
+            Debug.Assert(textProperty != null, "Text property for TextBlob relationship not found");
+
+            var value = (string)relationshipProperty.GetValue(element, null);
+            var textValue = value != null ? GetTextSerializer().Serialize(value) : null;
+
+            textProperty.SetValue(element, textValue, null);
+        }
+    }
+
+}
